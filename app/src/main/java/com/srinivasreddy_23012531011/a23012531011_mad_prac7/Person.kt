9ce547402512eef5f903cdcd4f2460723814660b
srinureddychereddy <srinureddychereddy@gmail.com>
2025-11-23 11:21:14 +0530

    prac 7

---
+++ app/src/main/java/com/srinivasreddy_23012531011/a23012531011_mad_prac7/Person.kt
@@ -0,0 +1,30 @@
+package com.srinivasreddy_23012531011.a23012531011_mad_prac7
+
+
+
+import org.json.JSONObject
+import java.io.Serializable
+
+class Person(
+    var id: String,
+    var name: String,var email: String,
+    var phone: String,
+    var address: String
+) : Serializable {
+
+    constructor(jsonObject: JSONObject) : this("", "", "", "", "") {
+        id = jsonObject.optString("id")
+        email = jsonObject.optString("email")
+        phone = jsonObject.optString("phone")
+
+        val profileJson = jsonObject.optJSONObject("profile")
+        if (profileJson != null) {
+            name = profileJson.optString("name")
+            address = profileJson.optString("address")
+        }
+    }
+
+    override fun toString(): String {
+        return "$name\n$email\n$phone\n$address"
+    }
+}
